b414fb36232272b12249c5cc85a9b79eb8196dba
Qingyu Feng <qyfeng86@hotmail.com>
2018-08-23 21:53:58 -0400

    Removed unnecessary files

--- PHPPart2SampleFiles/logout.php
+++
@@ -1,8 +0,0 @@
-<?
-  session_start();
-   session_destroy();
-
-   include("top.txt");
-   echo "Thank you for using the Workflow System.  You may <a href=\"login.php\">log in again</a>.";
-   include("bottom.txt");
-?>
